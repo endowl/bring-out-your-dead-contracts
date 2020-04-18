6b7f95557ed1ef6511d0e58f8a861f426bee3de2
morganstar <morganstar@gmail.com>
2020-04-18 02:03:39 -0700

    Update bring_out_your_dead_factory.sol
    Use CREATE2 in factory contract to deploy estates at determinable addresses

--- contracts/bring_out_your_dead_factory.sol
+++ contracts/bring_out_your_dead_factory.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.5.0;
 import "bring_out_your_dead.sol";
 
 contract BringOutYourDeadFactory {
+    event estateCreated(address indexed estate, address indexed owner);
+/*
     function newEstate(address oracle, address executor) public payable returns (address estateContract) {
         BringOutYourDead boyd = new BringOutYourDead();
         if(address(0) != oracle) {
@@ -12,7 +14,41 @@ contract BringOutYourDeadFactory {
             boyd.changeExecutor(executor);
         }
         boyd.transferOwnership(msg.sender);
+        emit estateCreated(address(boyd), msg.sender);
         return address(boyd);
     }
-}
+*/
 
+    // Use CREATE2 to create estate at predeterminable address
+    function newEstate(address oracle, address executor, uint256 salt) public payable returns (address payable estate) {
+        bytes memory code = type(BringOutYourDead).creationCode;
+        bytes32 newsalt = keccak256(abi.encodePacked(salt, msg.sender)); 
+        // address salt = msg.sender;
+        assembly {
+            estate := create2(0, add(code, 0x20), mload(code), newsalt)
+            if iszero(extcodesize(estate)) { revert(0, 0) }
+        }
+        if(address(0) != oracle) {
+            BringOutYourDead(estate).changeOracle(oracle);
+        }
+        if(address(0) != executor) {
+            BringOutYourDead(estate).changeExecutor(executor);
+        }
+        BringOutYourDead(estate).transferOwnership(msg.sender);
+        emit estateCreated(address(estate), msg.sender);
+    }    
+
+    function getEstateAddress(address creator, uint256 salt) public view returns (address estate) {
+        bytes memory code = type(BringOutYourDead).creationCode;
+        bytes32 newsalt = keccak256(abi.encodePacked(salt, creator)); 
+        bytes memory packed_bytecode = abi.encodePacked(code);
+        bytes32 temp = keccak256(abi.encodePacked(bytes1(0xff), address(this), bytes32(newsalt), bytes32(keccak256(packed_bytecode))));
+        uint mask = 2 ** 160 - 1;
+        assembly {
+            estate := and(temp, mask)
+        }
+        return estate;
+
+    }
+
+}
